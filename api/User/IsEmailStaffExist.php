b351c85acecaffb67e8de053752ca818f243250a
9thdat <nthdat09@gmail.com>
2024-01-01 23:40:36 +0700

    Fix validate token bug

--- api/User/IsEmailStaffExist.php
+++ api/User/IsEmailStaffExist.php
@@ -2,6 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json');
 
 // Respond to preflight requests
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
