2c64c292ae9fb5ebab6c536843b182fb1c32022d
9thdat <nthdat09@gmail.com>
2024-01-01 23:06:45 +0700

    add config

--- api/User/Login.php
+++ api/User/Login.php
@@ -3,6 +3,12 @@
 header('Access-Control-Allow-Origin: *');  // Replace with the actual origin of your frontend application
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json; charset=utf-8'); // Thêm header để chỉ định kiểu ký tự là UTF-8
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 include_once '../../config/db_azure.php';
 include_once '../../model/User.php';
